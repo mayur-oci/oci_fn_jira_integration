1e9bd29456cf8ce535945c9167c52e62784cfa95
Mayur <mayur.raleraskar@oracle.com>
2020-07-17 21:59:29 -0700

    trying another event

--- src/test/java/com/oci/fn/demo/FunctionOciEventToJsonTickeTest.java
+++ src/test/java/com/oci/fn/demo/FunctionOciEventToJsonTickeTest.java
@@ -13,11 +13,11 @@ public class FunctionOciEventToJsonTickeTest {
     @Rule
     public final FnTestingRule testing = FnTestingRule.createDefault();
 
-    //@Test
+    @Test
     public void testJiraTicketCreate() {
         FnHttpEventBuilder fnHttpEventBuilder = new FnHttpEventBuilder();
 
-        testing.givenEvent().withBody(getEventString()).enqueue();
+        testing.givenEvent().withBody(getObjCreatedEventString()).enqueue();
 
         testing.thenRun(OciEventProcessor.class, "handleRequest");
 
@@ -26,7 +26,7 @@ public class FunctionOciEventToJsonTickeTest {
         assertTrue(result.isSuccess());
     }
 
-    private String getEventString(){
+    private String getDbReminderEventString(){
        return
        "{\n" +
                "\t\"cloudEventsVersion\": \"0.1\",\n" +
@@ -65,4 +65,35 @@ public class FunctionOciEventToJsonTickeTest {
                "}" ;
     }
 
-}
+
+    private String getObjCreatedEventString() {
+        return "{\n" +
+                "  \"eventType\" : \"com.oraclecloud.objectstorage.createobject\",\n" +
+                "  \"cloudEventsVersion\" : \"0.1\",\n" +
+                "  \"eventTypeVersion\" : \"2.0\",\n" +
+                "  \"source\" : \"ObjectStorage\",\n" +
+                "  \"eventTime\" : \"2020-07-18T02:21:19Z\",\n" +
+                "  \"contentType\" : \"application/json\",\n" +
+                "  \"data\" : {\n" +
+                "    \"compartmentId\" : \"ocid1.compartment.oc1..aaaaaaaa2z4wup7a4enznwxi3mkk55cperdk3fcotagepjnan5utdb3tvakq\",\n" +
+                "    \"compartmentName\" : \"mayursandbox\",\n" +
+                "    \"resourceName\" : \"sddslave_qs_org.csv\",\n" +
+                "    \"resourceId\" : \"/n/intrandallbarnes/b/demoBucket/o/sddslave_qs_org.csv\",\n" +
+                "    \"availabilityDomain\" : \"IAD-AD-2\",\n" +
+                "    \"additionalDetails\" : {\n" +
+                "      \"bucketName\" : \"demoBucket\",\n" +
+                "      \"versionId\" : \"bb1a1623-57f7-495d-afdd-d25d5aff7f09\",\n" +
+                "      \"archivalState\" : \"Available\",\n" +
+                "      \"namespace\" : \"intrandallbarnes\",\n" +
+                "      \"bucketId\" : \"ocid1.bucket.oc1.iad.aaaaaaaaru7ge55pubirhhe3urvb65c7dhxexnbhljgm3h6cp7owk7bx6h7a\",\n" +
+                "      \"eTag\" : \"2d15a51e-0595-4b32-9dc6-f74454879401\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"eventID\" : \"be1c1a93-8856-328e-0527-d2e0375f49e2\",\n" +
+                "  \"extensions\" : {\n" +
+                "    \"compartmentId\" : \"ocid1.compartment.oc1..aaaaaaaa2z4wup7a4enznwxi3mkk55cperdk3fcotagepjnan5utdb3tvakq\"\n" +
+                "  }\n" +
+                "}";
+    }
+
+    }
